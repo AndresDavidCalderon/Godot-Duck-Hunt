d17fa4ac2bbbd57bb528bd3b9824f0ddab053116
Andres David Calderon <david.voidgames@gmail.com>
2023-09-10 11:36:40 -0500

    move ducks in the correct direction

--- duck.gd
+++ duck.gd
@@ -4,6 +4,9 @@ const SPEED := 250
 #var angle = Vector3.ZERO
 
 func _physics_process(delta):
-	velocity = -transform.basis.z * SPEED * delta
-	velocity = velocity * get_rotation()
+	var moving_axis = Vector3(-SPEED,0,0)
+	moving_axis=moving_axis.rotated(Vector3(0,1,0),rotation.y)
+	moving_axis=moving_axis.rotated(Vector3(1,0,0),rotation.x)
+	moving_axis=moving_axis.rotated(Vector3(0,0,1),rotation.z)
+	velocity = moving_axis * delta
 	move_and_slide()
